d6bb0c62f73b1b160610077ae874d3d78d490299
7DUCK7 <69091124+7DUCK7@users.noreply.github.com>
2023-08-30 17:10:03 +0300

    Add files via upload
    added custom fscanf functuion

--- SqEq.cpp
+++ SqEq.cpp
@@ -1,43 +1,48 @@
 #include "TXlib.h"
 #include "FUNCS.h"
 #include <stdio.h>
-#include "func.cpp"
-#include "test.cpp"
-int main()
+#include <math.h>
+#include <string.h>
+#include <assert.h>                                    //fix TestOne func using switch
+                                                       //static
+                                                       //scanf 40 symbols only
+                                                       //fix long lines make em readable
+                                                       //enum
+                                  // successful scanfs
+
+int main(int argc,const char* argv[])
 {
-    printf ("# Программа для решения квадратного уравнения\n");
-
-    for(int i = 0; i < nTESTS; i++)
+    TestData allData[nTESTS] = {{.a = 0, .b = 0,  .c = 0,  .x1 = 0,  .x2 = 0, .nROOTS = INF_ROOTS, "zero_arguments"},     // fix codestyle
+                                {.a = 1, .b = 0,  .c = -4, .x1 = -2, .x2 = 2, .nROOTS = 2,         "Basic_test1"},
+                                {.a = 0, .b = 0,  .c = 3,  .x1 = 0,  .x2 = 0, .nROOTS = 0,         "zero_roots"},
+                                {.a = 1, .b = -5, .c = 4,  .x1 = 0,  .x2 = 4, .nROOTS = 2,         "Basic_test2"}};
+if (argc != 2)
     {
-        TestOne(&allData[i]);
+        printf("Можно ввести только один аргумент командной строки (-test или -perform). Попробуйте заново.\n");
+        return 1;                                                                           //make if easier
     }
 
-    int where_to_read_from = 0;
-    double a = 0, b = 0, c = 0;
-
-    printf("Выберете способ которым хотите ввести данные:\n");
-    printf("1 - вручную\n");
-    printf("Любой другой символ - через файл\n");
-
-    FILE* how_to_read = stdin;
-    scanf("%d", &where_to_read_from);
-
-    how_to_read_coeffs(&a, &b, &c, where_to_read_from, how_to_read);
-
-    double x1 = 0, x2 = 0;
-    int nROOTS = SolveSqrEq (a, b, c, &x1, &x2);
-
-    switch (nROOTS)
+    if (Mystrcmp(argv[1], "-test") == 0)
     {
-    case ZERO: printf ("Решений нет");
-            break;
-    case ONE: printf ("x = %lg", x1);
-            break;
-    case TWO: printf ("x1 = %lg, x2 = %lg", x1, x2);
-            break;
-    case INF_ROOTS: printf("Бесконечное число корней");
-            break;
-    default: printf("main(): ERROR: nROOTS = %d\n", nROOTS);
+        for(int i = 0; i < nTESTS; i++)
+        {
+            TestOne(&allData[i]);
+        }
+        return 2;
+    }
+    else
+    {
+        if (Mystrcmp(argv[1], "-perform") == 0)
+        {
+            Perform();
+            return 3;
+
+        }
+        else
+        {
+            printf("Можно ввести только один аргумент командной строки (-test или -perform). Попробуйте заново.\n");
             return 1;
+        }
     }
+
 }
--- func.cpp
+++ func.cpp
@@ -1,6 +1,10 @@
+#include "TXlib.h"
+#include "FUNCS.h"
+#include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
-int CompareWithZero (double a)
+int IsItZero (double a)
 {
     if (fabs(a) <= isZERO)
         return 1;
@@ -10,28 +14,41 @@ int CompareWithZero (double a)
 
 int SolveLinearEq (double b, double c, double* x1)
 {
-    if (CompareWithZero(b))
+    assert(isfinite(b));
+    assert(isfinite(c));
+
+    if (x1 == 0)
     {
-        return (CompareWithZero(c))? INF_ROOTS : 0;
+        printf("Введён некорректный указатель для корня x1 в %s, в функции SolveLinearEq. Строка %d.\n", __FILE__, __LINE__);
     }
     else
     {
-     *x1 = -c / b;
-     return 1;
+        if (IsItZero(b))
+        {
+            return (IsItZero(c)) ? INF_ROOTS : 0;
+        }
+        else
+        {
+            *x1 = -c / b;
+            return 1;
+        }
     }
+    return 9;
 }
 
-int SolveSqrEq (double a, double b, double c, double* x1, double* x2)    // if for x pointer
+int SolveSqrEq (double a, double b, double c, double* x1, double* x2)
 {
-    // fix indentation
-    // float comparison
+    assert(isfinite(a));
+    assert(isfinite(b));
+    assert(isfinite(c));
+
     if (x1 == 0 || x2 == 0)
     {
-        printf("Введены некорректные указатели для корней уравнения\n");
+        printf("Введены некорректные указатели для корней уравнения в %s, в функции SolveSqrEq. Строка %d.\n", __FILE__, __LINE__);  // __LINE__
     }
     else
     {
-        if (CompareWithZero(a))
+        if (IsItZero(a))
         {
             return (SolveLinearEq (b, c, x1));
         }
@@ -48,7 +65,7 @@ int SolveSqrEq (double a, double b, double c, double* x1, double* x2)    // if f
             }
             else
             {
-                if (CompareWithZero(d))
+                if (IsItZero(d))
                 {
                     *x1 = -b / (2 * a);
                     return 1;
@@ -63,48 +80,45 @@ int SolveSqrEq (double a, double b, double c, double* x1, double* x2)    // if f
     return 0;
 
 }
-/*
-int ReadManual (double* a_pointer, double* b_pointer, double* c_pointer)
+
+int Read (double* a_pointer, double* b_pointer, double* c_pointer, FILE* chosen_type_of_input)          //fixxxxxxxxxxxx
 {
-    // check correctness +
-    printf ("# Введите коэффициенты уравнения\n");
-    while (fscanf (stdin, "%lg %lg %lg", a_pointer, b_pointer, c_pointer) != 3)    // correctness check
+    if (a_pointer == 0 || b_pointer == 0 || c_pointer == 0 || chosen_type_of_input == 0)
     {
-        while (getchar() != '\n')
-            ;
-        printf("Введены неверные данные (a= %lg b= %lg c= %lg)\n", *a_pointer, *b_pointer, *c_pointer);
+        printf("Введены неверные указатели в %s в функции Read. Строка %d.\n", __FILE__, __LINE__);
     }
-    return 1;
-}
-
-int ReadFile (double* a_pointer, double* b_pointer, double* c_pointer)
-{
-    FILE * file = fopen("RT.txt", "r");
-
-    while (fscanf(file, "a=%lg b=%lg c=%lg", a_pointer, b_pointer, c_pointer) != 3)
+    else
     {
-        while (getchar() != '\n')
-            ;
-        printf("Введены неверные данные (a= %lg b= %lg c= %lg)\n", *a_pointer, *b_pointer, *c_pointer);
-    }
+        int num_of_input = 1;
+        int successful_fscanfs = 0;
 
-    fclose(file);
-    return 1;
-}  */
+        while (successful_fscanfs != 3)
+        {
+            successful_fscanfs = fscanf (chosen_type_of_input, "%lg %lg %lg", a_pointer, b_pointer, c_pointer);
+            if (successful_fscanfs != 3)
+            {
 
-int Read (double* a_pointer, double* b_pointer, double* c_pointer, FILE* how_to_read_ptr)
-{
-    while (fscanf (how_to_read_ptr, "%lg %lg %lg", a_pointer, b_pointer, c_pointer) != 3)    // correctness check
-    {
-        while (getchar() != '\n')
-            ;
-        printf("Введены неверные данные (a=%lg b=%lg c=%lg). Повторите попытку:\n", *a_pointer, *b_pointer, *c_pointer);
+                printf("Введены неверные данные в %d попытке (a=%lg b=%lg c=%lg). Повторите попытку:\n",
+                    num_of_input, *a_pointer, *b_pointer, *c_pointer);
+                *a_pointer = 0; *b_pointer = 0; *c_pointer = 0;
+            }
+
+            num_of_input += 1;
+            buffer_clean(chosen_type_of_input);
+            if (successful_fscanfs == 3)
+                break;
+        }
+
+        return num_of_input - 1;
     }
-    return 1;
+    return num_of_input - 1;
 }
 
 int CompareDoubleNumbers(double number1, double number2)
 {
+    assert(isfinite(number1));
+    assert(isfinite(number2));
+
     if (fabs(number1 - number2) < isZERO)
         return 1;
     else
@@ -112,32 +126,169 @@ int CompareDoubleNumbers(double number1, double number2)
 
 }
 
-
-int how_to_read_coeffs(double* a_ptr, double* b_ptr, double* c_ptr, int where_to_read_from, FILE* how_to_read)
+void Print_the_answer(int nROOTS, double x1, double x2)
 {
-    if (where_to_read_from == 1)
-    {
-        printf ("# Введите коэффициенты уравнения\n");
-        while (getchar() != '\n')
-            ;
-        Read (a_ptr, b_ptr, c_ptr, how_to_read);
-        return 1;
-    }
-    else
-    {
-        char file_name[40] = {};
-        printf("Введите название файла с расширением:\n");
-        scanf("%s", file_name);
-        while ((how_to_read = fopen(file_name,"r")) == NULL)
-        {
-            printf("Имя файла введено неверно, повторите попытку:\n");
-            while (getchar() != '\n')
-                ;
-            scanf("%s", file_name);
-        }
-        Read (a_ptr, b_ptr, c_ptr, how_to_read);
-        return 2;
+    assert(isfinite(nROOTS));
+    assert(isfinite(x1));
+    assert(isfinite(x2));
 
+    switch (nROOTS)
+    {
+    case ZERO:
+            printf ("Решений нет\n");
+            break;
+    case ONE:
+            printf ("x = %lg\n", x1);
+            break;
+    case TWO:
+            printf ("x1 = %lg, x2 = %lg\n", x1, x2);
+            break;
+    case INF_ROOTS:
+            printf("Бесконечное число корней\n");
+            break;
+    default:
+            printf("main(): ERROR: nROOTS = %d\n", nROOTS);
     }
 
 }
+
+int get_file_name(char* file_name)
+{
+    printf("Введите название файла с расширением:\n");
+    buffer_clean(stdin);
+    scan_file_name(file_name);
+    while (fopen(file_name ,"r") == NULL)
+        {
+            printf("Имя файла введено неверно, повторите попытку:\n");
+            //printf(file_name);
+            //buffer_clean(stdin);
+            scan_file_name(file_name);
+                                                         //write this func urself using for i...
+        }
+    return 1;
+}
+
+
+int get_type_of_the_input_from_user(INPUT_TYPE* where_to_read_from)
+{
+    scanf("%d", where_to_read_from);
+    while((*where_to_read_from != FROM_FILE) && (*where_to_read_from != FROM_CONSOLE))
+    {
+        buffer_clean(stdin);
+        printf("Введена неверная команда\n"
+               "Попробуйте заново\n");
+        scanf("%d", where_to_read_from);
+    }
+    return 1;
+}
+
+int Perform()
+{
+        INPUT_TYPE where_to_read_from = NOT_CHOSEN_YET;
+        FILE* chosen_type_of_input;      //fix names of variables
+        char file_name[FILE_NAME_SIZE] = "";
+        double a = 0, b = 0, c = 0;
+
+        first_msg();
+
+        get_type_of_the_input_from_user(&where_to_read_from);
+
+        int num_of_successful_solution = Read(&a, &b, &c, choose_how_to_read(where_to_read_from, file_name, chosen_type_of_input));
+
+        printf("В ходе попытки %d:\n", num_of_successful_solution);
+
+        double x1 = 0, x2 = 0;
+        int nROOTS = SolveSqrEq (a, b, c, &x1, &x2);
+
+        Print_the_answer(nROOTS, x1, x2);
+        return 3;
+}
+
+FILE* choose_how_to_read(INPUT_TYPE where_to_read_from, char* file_name, FILE* chosen_type_of_input)
+{
+    if (where_to_read_from == FROM_FILE)
+    {
+        get_file_name(file_name);
+        chosen_type_of_input = fopen(file_name,"r");
+    }
+    else
+    {
+        chosen_type_of_input = stdin;
+        printf("Введите коэффициенты уравнения\n");
+    }
+    return chosen_type_of_input;
+}
+
+int Mystrcmp(const char* str1, const char* str2)
+{
+    int j = 0;
+    while(str1[j] != '\0' || str2[j] != '\0')
+    {
+        if (str1[j] != str2[j])
+        {
+            return 1;
+        }
+        j += 1;
+    }
+    return 0;
+}
+
+void buffer_clean(FILE* chosen_type_of_input)
+{
+    while (getc(chosen_type_of_input) != '\n')
+                    ;
+}
+
+void first_msg()
+{
+    printf ("# Программа для решения квадратного уравнения\n");
+    printf("Выберете способ которым хотите ввести данные:\n");
+    printf("1 - вручную\n");
+    printf("2 - через файл\n");
+}
+
+
+void scan_file_name(char* name)
+{
+    for (int i = 0; i < FILE_NAME_SIZE; i++)
+    {
+        name[i] = getchar();
+        if (name[i] == '\n')
+        {
+            name[i] = '\0';
+            break;
+        }
+    }
+}
+/*
+void Inizialization(int argc, char* argv)
+{
+    if (argc != 2)
+    {
+        printf("Можно ввести только один аргумент командной строки (-test или -perform). Попробуйте заново.\n");
+        return 1;                                                                           //make if easier
+    }
+
+    if (Mystrcmp(argv[1], "-test") == 0)
+    {
+        for(int i = 0; i < nTESTS; i++)
+        {
+            TestOne(&allData[i]);
+        }
+        return 2;
+    }
+    else
+    {
+        if (Mystrcmp(argv[1], "-perform") == 0)
+        {
+            Perform();
+            return 3;
+
+        }
+        else
+        {
+            printf("Можно ввести только один аргумент командной строки (-test или -perform). Попробуйте заново.\n");
+            return 1;
+        }
+    }
+}   */
